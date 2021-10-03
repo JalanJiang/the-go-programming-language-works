695706dea9b394c7edfec5d1c719c67e31298db5
JalanJiang <448300947@qq.com>
2021-10-03 20:09:34 +0800

    [exercise] 1.9 HTTPCode: resp.Status

---
+++ ch1/exercise1.9/main.go
@@ -0,0 +1,37 @@
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		// 判断是否传入协议前缀
+		hasPrefix := strings.HasPrefix(url, "http://")
+		if !hasPrefix {
+			// 补充前缀
+			url = "http://" + url
+		}
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		// 复制到 os.Stdout 里
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		// 获取 http 状态码
+		httpCode := resp.Status
+		fmt.Println(httpCode)
+	}
+}
